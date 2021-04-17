0e8fe7231d072b696e201ab9fe250f900fa87e5e
marchuanv <marchuanv@gmail.com>
2021-04-17 03:22:37 +0200

    refactoring

--- component.messagebus.js
+++ component.messagebus.js
@@ -1,8 +1,2 @@
 const { MessageBus } = require("./lib/messagebus.js");
-process.on('SIGTERM', () => saveCallstack() );
-process.on('exit', () => saveCallstack() );
-process.on('SIGINT', () => saveCallstack() );
-process.on('SIGUSR1', () => saveCallstack() );
-process.on('SIGUSR2', () => saveCallstack() );
-process.on('uncaughtException', () => saveCallstack() );
 module.exports = { MessageBus };
